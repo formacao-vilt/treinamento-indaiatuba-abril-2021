8ed33ecc5e6f7d74d449cf962f79de54433c36b7
Cassiel Rattes Cortez <cassielrattes@gmail.com>
2021-04-26 15:14:48 -0300

    Modificando a validação de formulário

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 import initMenuMobile from "./modules/menu-mobile.js";
 import InitValidar from "./modules/validar.js";
-import InitValidarTest from "./modules/validation_test.js";
 
 initMenuMobile();
 InitValidar();
 
-InitValidarTest()
