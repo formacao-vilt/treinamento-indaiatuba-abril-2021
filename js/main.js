6f6d413f1df11c8f1c5deebd179fdb5aeeee4740
maria31012 <maria31012@fiec.edu.br>
2021-04-28 09:24:14 -0300

    alteraçao na validaçao do formulario

--- js/main.js
+++ js/main.js
@@ -12,33 +12,64 @@ function validar() {
     msg += "- Nome completo\n";
     nome.style.outline = "2px solid red"
   }
+  else {
+    nome.style.outline = ""
+  }
+
   if (cpf.value == "") {
     msg += "- CPF\n";
     cpf.style.outline = "2px solid red"
   }
+  else {
+    cpf.style.outline = ""
+  }
+
   if (data.value == "") {
     msg += "- Nascimento\n";
     data.style.outline = "2px solid red"
   }
+  else {
+    data.style.outline = ""
+  }
+
   if (sexo.value == "") {
     msg += "- Sexo\n";
     sexo.style.outline = "2px solid red"
   }
+  else {
+    sexo.style.outline = ""
+  }
+
   if (email.value == "") {
     msg += "- Email\n";
     email.style.outline = "2px solid red"
   }
+  else {
+    email.style.outline = ""
+  }
+
   if (celular.value == "") {
     msg += "- Celular\n";
     celular.style.outline = "2px solid red"
   }
+  else {
+    celular.style.outline = ""
+  }
+
   if (nivel.value == "") {
     msg += "- Nível\n";
     nivel.style.outline = "2px solid red"
   }
+  else {
+    nivel.style.outline = ""
+  }
+
   if (media.value == "") {
     msg += "- Média\n";
     media.style.outline = "2px solid red"
+  }
+  else {
+    media.style.outline = ""
   }
 
   alert(msg);
