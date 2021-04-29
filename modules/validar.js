a3ab6a83cac7a3f24deb9ab6aaa38ef5ad9e6d08
Cassiel Rattes Cortez <cassielrattes@gmail.com>
2021-04-29 19:52:13 -0300

    Adicionando Bootstrap

--- modules/validar.js
+++ modules/validar.js
@@ -1,5 +1,4 @@
 export default function InitValidar() {
-
   const form = document.getElementById("cadastro");
   const nome = document.getElementById("nome");
   const cpf = document.getElementById("cpf");
@@ -11,41 +10,56 @@ export default function InitValidar() {
   const media = document.getElementById("media");
   const errorElement = document.getElementById("error");
 
-  form.addEventListener("submit", e => {
-    e.preventDefault()
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
     let msg = [];
-    const emailRegex = new RegExp(
-      "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-    );
-    const cpfRegex = new RegExp("^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}");
-  
-    if (nome.value === "" || nome.value.length < 3) msg.push("Nome é obrigatório e deve ter no minimo 3 caracteres");
-    if (
-      cpf.value === "" ||
-      cpf.value.replace(/[^\d]+/g, "").length > 11 ||
-      !cpfRegex.test(cpf.value)
-    ) msg.push("CPF invalido ou não preenchido");
-    if (email.value === "" || !emailRegex.test(email.value)) msg.push("E-mail Invalido ou não preenchido");
-    if (data.value === "") msg.push("Data de Nascimento é obrigatório");
-    if (sexo.value === "") msg.push("Sexo é obrigatório");
-    if (celular.value === "") msg.push("Celular é obrigatório");
-    if (nivel.value === "") msg.push("Nível é obrigatório");
-    if (media.value === "") msg.push("Média é obrigatório");
 
-    if (msg.length > 0) errorElement.innerText = msg.join(",\n") 
+    const reg = {
+      emailRegex: new RegExp(
+        "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+      ),
+      cpfRegex: new RegExp("^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}"),
+    };
+
+    const msgError = {
+      nome: "Nome é obrigatório e deve ter no minimo 3 caracteres",
+      cpf: "CPF invalido ou não preenchido",
+      email: "E-mail Invalido ou não preenchido",
+      data: "Data de Nascimento é obrigatório",
+      sexo: "Sexo é obrigatório",
+      celular: "Celular é obrigatório",
+      nivel: "Nível é obrigatório",
+      media: "Média é obrigatório",
+    };
+
+    if (nome.value.length < 3) msg.push(msgError.nome);
+    if (
+      cpf.value == "" ||
+      cpf.value.replace(/[^\d]+/g, "").length > 11 ||
+      !reg.cpfRegex.test(cpf.value)
+    )
+      msg.push(msgError.cpf);
+
+    if (email.value == "" || !reg.emailRegex.test(email.value))
+      msg.push(msgError.email);
+    if (data.value == "") msg.push(msgError.data);
+    if (sexo.value == "") msg.push(msgError.sexo);
+    if (celular.value == "") msg.push(msgError.celular);
+    if (nivel.value == "") msg.push(msgError.nivel);
+    if (media.value == "") msg.push(msgError.media);
+
+    if (msg.length > 0) errorElement.innerText = msg.join(",\n");
     else {
       errorElement.innerText = "";
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json; charset=UTF-8",
         },
         body: JSON.stringify({
-          title: email.value
-        })
+          title: email.value,
+        }),
       });
-      
     }
-  })
+  });
 }
