94300249f1ab2293724b12c31440fd7de65b893e
matheusoliveira2003 <oliveiramatheuspaulo@gmail.com>
2021-05-14 05:14:36 -0300

    14/05/2021

--- js/main.js
+++ js/main.js
@@ -1,62 +1,46 @@
 function validar() {
-    // ARMAZENA O VALOR DE CADA ELEMENTO SELECIONADO PELO ID INSERIDO NO ARQUIVO HTML
-    var nome = document.getElementById("nome").value;
-    var cpf = document.getElementById("cpf").value;
-    var nasc = document.getElementById("data").value;
-    var sexo = document.getElementById("sexo").value;
-    var email = document.getElementById("email").value;
-    var cel = document.getElementById("celular").value;
-    var nivel = document.getElementById("nivel").value;
-    var media = document.getElementById("media").value;
-    
-    //SE O USUÁRIO NÃO INSERIU OS ELEMENTOS, O PROGRAMA O AVISARÁ! ASSIM QUE INSERIDOS, O AVISO DESAPARECE. 
+  var nome = document.getElementById("nome");
+  var cpf = document.getElementById("cpf");
+  var email = document.getElementById("email");
+  var data = document.getElementById("data");
+  var celular = document.getElementById("celular");
+  var sexo = document.getElementById("sexo");
+  var styles = { border: "2px solid red" }
+  var msg = "Por favor preencha o(s) campo(s) abaixo:\n\n";
 
-    if(nome == "") {
-        document.getElementById('errorNome').innerHTML = "Insira um nome!";
-    }
-    else {
-        document.getElementById('errorNome').innerHTML = "";
-    }
-    if(cpf == "") {
-        document.getElementById('errorCpf').innerHTML = "Insira um CPF válido!";
-    }
-    else {
-        document.getElementById('errorCpf').innerHTML = "";
-    }
-    if(nasc == "") {
-        document.getElementById('errorData').innerHTML = "Insira uma data de nascimento!";
-    }
-    else {
-        document.getElementById('errorData').innerHTML = "";
-    }
-    if(sexo == "") {
-        document.getElementById('errorSexo').innerHTML = "Selecione um sexo!";
-    }
-    else {
-        document.getElementById('errorSexo').innerHTML = "";
-    }
-    if(email == "") {
-        document.getElementById('errorEmail').innerHTML = "Insira um endereço de email!";
-    }
-    else {
-        document.getElementById('errorEmail').innerHTML = "";
-    }
-    if(cel == "") {
-        document.getElementById('errorCel').innerHTML = "Insira um número de celular!";
-    }
-    else {
-        document.getElementById('errorCel').innerHTML = "";
-    }
-    if(nivel == "") {
-        document.getElementById('errorNivel').innerHTML = "Selecione seu nível!";
-    }
-    else {
-        document.getElementById('errorNivel').innerHTML = "";
-    }
-    if(media == "") {
-        document.getElementById('errorMedia').innerHTML = "Insira sua média!";
-    }
-    else {
-        document.getElementById('errorMedia').innerHTML = "";
-    }
+  if (nome.value == "") {
+    msg += "- Nome completo\n";
+    nome.style.outline = "2px solid red"
+  }
+  if (cpf.value == "") {
+    msg += "- CPF\n";
+    cpf.style.outline = "2px solid red"
+  }
+  if (data.value == "") {
+    msg += "- Nascimento\n";
+    data.style.outline = "2px solid red"
+  }
+  if (sexo.value == "") {
+    msg += "- Sexo\n";
+    sexo.style.outline = "2px solid red"
+  }
+  if (email.value == "") {
+    msg += "- Email\n";
+    email.style.outline = "2px solid red"
+  }
+  if (celular.value == "") {
+    msg += "- Celular\n";
+    celular.style.outline = "2px solid red"
+  }
+  if (nivel.value == "") {
+    msg += "- Nível\n";
+    nivel.style.outline = "2px solid red"
+  }
+  if (media.value == "") {
+    msg += "- Média\n";
+    media.style.outline = "2px solid red"
+  }
+
+  alert(msg);
+
 }
